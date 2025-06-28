09f9b605f5ef2a69ae9027929a79cfb718931b45
Анатолий <TOLRH333@gmail.com>
2025-06-29 09:38:44 +1000

    Update 0.2
    Добавлена возможность покормить животное.
    Вся основа проекта была заложена и полностью функционирует.

--- ZooApplicationConsole/Functional/Actions.cs
+++ ZooApplicationConsole/Functional/Actions.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text;
 using ZooApplicationConsole.Animals;
 using ZooApplicationConsole.Animals.Factory;
 using ZooApplicationConsole.Data;
@@ -32,37 +30,74 @@ namespace ZooApplicationConsole.Functional
 
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Выберите тип животного:");
+            Console.Write("Выберите тип животного: ");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(sb.ToString());
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Ваш тип:");
+            Console.Write("Ваш тип: ");
             Console.ForegroundColor = ConsoleColor.Blue;
             var animalType = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Введите имя животного:");
+            Console.Write("Введите имя животного: ");
             Console.ForegroundColor = ConsoleColor.Blue;
             var name = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Введите звук вашего животного:");
+            Console.Write("Введите звук вашего животного: ");
             Console.ForegroundColor = ConsoleColor.Blue;
             var sound = Console.ReadLine();
 
-            if (animalType == null || name == null || sound == null)
+            if (
+                string.IsNullOrWhiteSpace(animalType) ||
+                string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(sound)
+                )
                 Console.WriteLine(
                     "Какой-то из параметров был указан неверно!\n" +
+                    "Поля не должны быть пустыми или состоять из одних пробелов.\n" +
                     "Попробуйте снова с другими параметрами."
                     );
             else
             {
                 var factory = _factoryProvider.GetFactoryByKey(animalType);
+                if (factory == null) return;
+
                 var animal = factory?.CreateAnimal(name, sound);
                 _animalsData.AddNewAnimal(animal);
-
                 Console.WriteLine("Животное успешно добавленно в вашу коллекцию");
             }
         }
-        public void FeedAnimal() { }
+        public void FeedAnimal() 
+        {
+            WatchAnimalsList();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Выберите животное, которое хотите покормить: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            
+             int num;
+            var animals = _animalsData.GetAnimals();
+            
+            if (int.TryParse(Console.ReadLine(), out num) 
+                && num <= animals.Count
+                && num > 0)
+            {
+                num -= 1;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(
+                    $"Вы выбрали: {animals[num].Name}\n" +
+                    $"Его энергия: {animals[num].Energy}\n");
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen; 
+                animals[num].TakeFood();
+                Console.WriteLine($"Энергия после кормления: {animals[num].Energy}\n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка! Неверено указанно животное, попробуйте снова.");
+            }
+                    
+        }
         public void WatchAnimalsList() 
         {
             StringBuilder sb = new StringBuilder();
